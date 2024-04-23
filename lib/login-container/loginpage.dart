2ccb37d0322b599c02d768e0207c5144788ebde3
DOHA <koo9811@gmail.com>
2024-04-23 16:49:25 +0900

    login

---
+++ lib/login-container/loginpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatelessWidget {
+  const Loginpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Logintest page'),
+      ),
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import '../login-container/loginpage.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -16,8 +18,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'MemoApp',
+    return MaterialApp(
+      title: 'Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Loginpage(),
     );
   }
 }
