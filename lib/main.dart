8740fb57f4ef2b3af958a7a81979d2f1fb88f094
DOHA <koo9811@gmail.com>
2024-04-28 22:40:44 +0900

    backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:logger/logger.dart';
 
 // 대기페이지 가져오기
 import 'Container/auth.dart' as login;
@@ -16,6 +18,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //서버 켜기
+  final server = const MyServerWidget();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,5 +30,38 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
     );
+  }
+}
+
+class MyServerWidget extends StatefulWidget {
+  const MyServerWidget({super.key});
+
+  @override
+  MyServerWidgetState createState() => MyServerWidgetState();
+}
+
+class MyServerWidgetState extends State<MyServerWidget> {
+  final logger = Logger();
+  late io.Socket socket;
+
+  @override
+  void initState() {
+    super.initState();
+    _connectToServer();
+  }
+
+  void _connectToServer() {
+    socket = io.io('http://localhost:3000');
+    socket.onConnect((_) {
+      socket.emit('msg', 'test');
+    });
+
+    socket.on('event', (data) => logger.i(data));
+    socket.onDisconnect((_) => logger.e('disconnect'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(); // 빈 컨테이너를 반환
   }
 }
