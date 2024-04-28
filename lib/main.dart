82ae3778465916b9190f831637460d0df42086ed
DOHA <koo9811@gmail.com>
2024-04-29 01:11:22 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,25 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 import 'package:logger/logger.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
 
 // 대기페이지 가져오기
 import 'Container/auth.dart' as login;
 
+Response _echoRequest(Request request) {
+  return Response.ok('You said: ${request.url}');
+}
+
 void main() async {
+  final logger = Logger();
+  // 서버 생성
+  final handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(_echoRequest);
+
+  final server = await io.serve(handler, 'localhost', 3000);
+  logger.i('Serving at http://${server.address.host}:${server.port}');
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -15,32 +29,14 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  //서버 켜기
-  final server = const MyServerWidget();
-
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Game',
-      home: const login.Loginpage(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyServerWidget extends StatefulWidget {
-  const MyServerWidget({super.key});
-
-  @override
-  MyServerWidgetState createState() => MyServerWidgetState();
-}
-
-class MyServerWidgetState extends State<MyServerWidget> {
+class _MyAppState extends State<MyApp> {
   final logger = Logger();
   late io.Socket socket;
 
@@ -54,6 +50,7 @@ class MyServerWidgetState extends State<MyServerWidget> {
     socket = io.io('http://localhost:3000');
     socket.onConnect((_) {
       socket.emit('msg', 'test');
+      logger.i('socket_connect');
     });
 
     socket.on('event', (data) => logger.i(data));
@@ -62,6 +59,12 @@ class MyServerWidgetState extends State<MyServerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(); // 빈 컨테이너를 반환
+    return MaterialApp(
+      title: 'My Game',
+      home: const login.Loginpage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
   }
 }
